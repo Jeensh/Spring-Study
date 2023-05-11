9a8a09b4df16bea7760e66ed317520e089b4379b
신동해 <jeensh25@gmail.com>
2023-05-11 18:09:06 +0900

    (완료) 스프링 입문 - 스프링 DB 접근 기술 섹션 - 프로젝트 생성

---
+++ infrun_enter/enter_spring/src/test/java/hello/enter_spring/EnterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.enter_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
