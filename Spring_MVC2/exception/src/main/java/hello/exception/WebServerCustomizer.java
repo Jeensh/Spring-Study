afa6733ecf5149e7553d6da6ed949af3ace67e0b
신동해 <jeensh25@gmail.com>
2023-06-03 15:32:36 +0900

    API 예외 처리

--- Spring_MVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ Spring_MVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -2,12 +2,11 @@ package hello.exception;
 
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
